1dcd91e6ebcf72c17d3bdbedfc2084daf8ee116c
734692305 <liuhuijun_web@sina.com>
2020-08-04 18:53:48 +0800

    动态路由

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+<p>About Page</p>
+<!-- a 链接，刷新导航，走服务端渲染 -->
+<h2>a链接</h2>
+<a href="/">首页</a>
+
+<!-- router-link  导航连接组件 -->
+<h2>router-link</h2>
+<router-link to='/'>首页</router-link>
+
+<!-- 编程式导航 -->
+<button @click="onClick">首页</button>
+</div>
+</template>
+
+<script>
+export default {
+name: "aboutPage",
+methods: {
+
+  onClick() {
+    this.$router.push('/')
+  }
+}
+}
+</script>
+
+<style scoped>
+</style>
